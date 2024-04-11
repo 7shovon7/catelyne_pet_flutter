1fb1f6a7b2ba0f1f4778e54d1eef01b4384487b7
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-04-11 06:02:08 +0600

    example items modified

--- lib/ui/pages/home_page/sections/home_best_sellers_section.dart
+++ lib/ui/pages/home_page/sections/home_best_sellers_section.dart
@@ -7,26 +7,29 @@ import 'package:google_fonts/google_fonts.dart';
 
 List<ProductViewModel> allProducts = [
   ProductViewModel(
-    title: 'Product 1',
-    description: 'Description of Product 1',
-    category: 'Category 1',
-    price: 50.0,
+    title: 'Interactive Cat Toy',
+    description:
+        'Interactive Robotic Cat Toy, Automatic Feather/Ball Teaser Toy with 2 Replacements',
+    category: 'Cat Toys',
+    price: 18.99,
     imgUrl:
         'https://th.bing.com/th/id/R.2d4c2935a93b8728effdaaf81bb510e4?rik=YDsazFbHww508g&pid=ImgRaw&r=0',
   ),
   ProductViewModel(
-    title: 'Product 2',
-    description: 'Description of Product 2',
-    category: 'Category 2',
-    price: 60.0,
+    title: 'Dog Chew Toys',
+    description:
+        'Dog Chew Toys for Aggressive Chewers, Tough Indestructible Dog Toys for Large Dogs',
+    category: 'Dog Toys',
+    price: 14.99,
     imgUrl:
         'https://th.bing.com/th/id/R.2d4c2935a93b8728effdaaf81bb510e4?rik=YDsazFbHww508g&pid=ImgRaw&r=0',
   ),
   ProductViewModel(
-    title: 'Product 3',
-    description: 'Description of Product 3',
-    category: 'Category 1',
-    price: 70.0,
+    title: 'Cat Tree Tower',
+    description:
+        'Multi-Level Cat Tree Tower with Scratching Posts, Perch, Condo, Hammock and Dangling Balls',
+    category: 'Cat Furniture',
+    price: 49.99,
     imgUrl:
         'https://th.bing.com/th/id/R.2d4c2935a93b8728effdaaf81bb510e4?rik=YDsazFbHww508g&pid=ImgRaw&r=0',
   ),
@@ -103,7 +106,7 @@ List<Widget> homePageBestSellersSectionWidgets(BuildContext context) => [
         ),
       ),
       CsGridView(
-        maxItemsPerRow: 4,
+        maxItemsPerRow: 3,
         children: List.generate(
           allProducts.length,
           (index) => CsProductCard(product: allProducts[index]),
