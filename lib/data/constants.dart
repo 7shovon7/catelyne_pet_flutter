d287ea7dbd8262fc0fe168cef6268d7f1434773b
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-04-14 01:12:05 +0600

    data initiated

---
+++ lib/data/constants.dart
@@ -0,0 +1,25 @@
+abstract class DataConstants {
+  static const List<String> menuItems = [
+    'Home',
+    'Products',
+    'Pet Profile',
+    'Blog',
+    'About Us',
+  ];
+
+  static const List<String> productsSubMenuItems = [
+    'Cat Food',
+    'Cat Litter',
+    'Health & Grooming',
+    'Toy',
+    // 'Miscellaneous',
+  ];
+
+  static const List<String> catFoodTypes = [
+    'Dry Food',
+    'Wet Food',
+    'Pouch',
+    'Can Food',
+    'Treats',
+  ];
+}
