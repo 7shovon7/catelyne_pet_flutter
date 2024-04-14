a6753bd600e6e1dc0c63b51a74318ea64be2bf07
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-04-14 08:52:52 +0600

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 // import 'dart:html' as html;
+import 'package:catelyne_pet_flutter/firebase_options.dart';
 import 'package:catelyne_pet_flutter/ui/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:meta_seo/meta_seo.dart';
 
-void main() {
+void main() async {
   if (kIsWeb) {
     MetaSEO().config();
-    // MetaSEO().author(author: 'Mahmudur Rahman Shovon');
-    // html.MetaElement meta = html.MetaElement();
-    // meta.name = 'meta-name';
-    // meta.content = 'meta-content';
-    // html.document.head?.append(meta);
   }
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
